68e51a0cac31c0ac1f82aba538de3e61ea453fdb
speedvip1 <1speedhubx@gmail.com>
2025-10-20 18:44:47 +0300

    Rename AddDiscordInvite to NewDiscord

--- Libnew.lua
+++ Libnew.lua
@@ -1482,7 +1482,7 @@ function Kavo.CreateLib(kavName, themeList)
                     end
                 end)        
             end
-    function Elements:AddDiscordInvite(Configs)
+    function Elements:NewDiscord(Configs)
     local Title = Configs[1] or Configs.Name or Configs.Title or "Discord"
     local Desc = Configs.Desc or Configs.Description or ""
     local Logo = Configs[2] or Configs.Logo or ""
